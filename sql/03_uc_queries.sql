caa80da369865de0d55b0c7e629e81d687d97f90
smanaspratap <manaspratap2003@gmail.com>
2026-03-13 16:34:39 +0530

    [Aaditya]: UC6: Added count by City and State queries

--- sql/03_uc_queries.sql
+++ sql/03_uc_queries.sql
@@ -23,3 +23,14 @@ WHERE city = 'Mumbai';
 -- Retrieve Person belonging to a State from the Address Book 
 SELECT * FROM address_book 
 WHERE state = 'Maharashtra';
+USE address_book_service;
+
+-- Ability to understand the size of address book by City
+SELECT city, COUNT(*) as city_count 
+FROM address_book 
+GROUP BY city;
+
+-- Ability to understand the size of address book by State
+SELECT state, COUNT(*) as state_count 
+FROM address_book 
+GROUP BY state;
